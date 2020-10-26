188b6c7ed2389f6f135d70744fb19622b3cfdb94
jonespi <jonathan.espinosa@gmail.com>
2020-10-25 21:26:24 -0400

    feature complete, styling

--- src/App.js
+++ src/App.js
@@ -1,39 +1,46 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useState } from 'react';
 import { getLinkData } from  './services'
 import { parseTextMap } from './utils'
 
 const App = () => {
   const [url, setUrl] = useState(null);
-  const [clicked, setClicked] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [textView, setTextView] = useState(null);
   const [mostUsed, setMostUsed] = useState(null);
   const [highlightedWord, setHighlightedWord] = useState(null);
+  const [hasError, setHasError] = useState(false);
   
-  useEffect(() =>  {
-    
-  }, []);
-
   const displayTextView = async (e) => {
     e.preventDefault();
-    setClicked(true);
+    setIsLoading(true);
     resetState();
-    const text = await getLinkData(url);
-    setTextView(text);
-    parseMap(text);
-    setClicked(false);
+    try { 
+      const text = await getLinkData(url); 
+      if (text.length > 0) {
+        setTextView(text);
+        parseMap(text);
+        setIsLoading(false);
+      } else {
+        setHasError(true);
+        setIsLoading(false);
+      }
+    } catch {
+      setHasError(true);
+      setIsLoading(false);
+    }
   };
 
   const parseMap = (text) => {
     const map = parseTextMap(text);
-    if (text !== []) {
+    if (map.length > 0) {
       setMostUsed(map);
     } else {
-      setMostUsed('No data');
+      setMostUsed(null);
     }
   }
 
   const resetState = () => {
+    setHasError(false);
     setTextView(null);
     setMostUsed(null);
     setHighlightedWord(null);
@@ -41,51 +48,132 @@ const App = () => {
 
   return (
     <div className="App">
-      <form onSubmit={(e) =>displayTextView(e)}>
+      <form onSubmit={(e) => displayTextView(e)}>
+        <h1>Enter a URL</h1>
         <input 
           type="text" 
-          placeholder="Enter a url"
+          className="url-text"
+          placeholder="http://cushion.ai"
           onChange={e => setUrl(e.target.value)}
           />
-        {!clicked ? (
-          <input type="submit" />
+        {!isLoading ? (
+          <input 
+            disabled={!url}
+            className="submit-button"
+            type="submit" />
         ) : (
           <p>LOADING...</p>
           )
         }
       </form>
-      {textView && (
-        <div>
-          {textView.map(word => {
-            return (
-              <>
-                <span 
-                  className={highlightedWord === word.toLowerCase() && 'highlighted'}
-                  onClick={() => setHighlightedWord(word.toLowerCase())}>
-                  {word} {' '}
-                </span>
-              </>
-            ) 
-          })}</div>
-      )}
-      {mostUsed && (
-        <div>
-        <h2>Most Used Words</h2>
-        <p>Word - Times Used</p>
-        {mostUsed.map(word => {
-          return (
-            <p onClick={() => setHighlightedWord(word[0].toLowerCase())}>
-              {word[0]}: {word[1]}
-            </p>
-          )}
+      <div className="content">
+        {textView && (
+          <div className="text-view">
+            {textView.map(word => {
+              return (
+                <>
+                  <span 
+                    className={highlightedWord === word.toLowerCase() && 'highlighted'}
+                    onClick={() => setHighlightedWord(word.toLowerCase())}>
+                    {word}
+                  </span>
+                  {' '}
+                </>
+              ) 
+            })}
+          </div>
         )}
-        </div>
+        {mostUsed && (
+          <div className="most-used">
+            <h2>Most Used Words</h2>
+            <h3 className="most-used-subheader">
+              <span>Word {' '}</span> 
+              <span>Times Used</span>  
+            </h3>
+            {mostUsed.map(word => {
+              return (
+                <div
+                  className={`most-used-word ${highlightedWord === word[0].toLowerCase() && 'highlighted'}`} 
+                  onClick={() => setHighlightedWord(word[0].toLowerCase())}>
+                  <span>{word[0]}</span> 
+                  <span>{word[1]}</span>
+                </div>
+              )})}
+          </div>
+        )}
+      </div>
+      {hasError && (
+        <div className="error">There was an issue with your URL. Please try a different one or try again.</div>
       )}
       <style jsx>
         {`
+          * {
+            font-family: 'Roboto', sans-serif;
+          }
+          form {
+            display: flex;
+            align-items: center;
+            margin: 0 auto;
+            padding: 0 auto;
+            flex-direction: column;
+            margin-top: 25px;
+            margin-bottom: 20px;
+          }
+          .content {
+            margin: 0 auto;
+            padding: 0 auto;
+            max-width: 1024px;
+            height: 60vh;
+            display: flex;
+          }
+          .text-view {
+            flex: 2;
+            height: 800px;
+            overflow: scroll;
+          }
+          .text-view span {
+            cursor: pointer;
+          }
+          .text-view::-webkit-scrollbar {
+            display: none;
+          }
+          .most-used {
+            flex: 1;
+            margin-left: 10px;
+            padding-right: 20px;
+          }
+          .most-used-subheader, .most-used-word {
+            width: 300px;
+            display: flex;
+            justify-content: space-between;
+          }
+          .most-used-word {
+            margin: 10px 0;
+            padding: 5px 0;
+            cursor: pointer;
+          }
+          .url-text {
+            width: 350px;
+            margin-bottom: 10px;
+          }
+          .submit-button {
+            width: 250px;
+            padding: 5px;
+            border-radius: 5px;
+          }
+          .submit-button:not([disabled]):hover {
+            background-color: lightblue;
+            cursor: pointer;
+          }
           .highlighted {
+            font-weight: 900;
             background-color: yellow;
             color: red;
+          }
+          .error {
+            margin: 0 auto;
+            text-align: center;
+            max-width: 1024px;
           }
         `}
       </style>
--- src/services.js
+++ src/services.js
@@ -2,22 +2,16 @@ import axios from 'axios';
 import { hasTextContent, noNewLines } from './utils'
 
 export const getLinkData = async (url) => {
-    try {
-        const response = await axios.get(`https://cors-anywhere.herokuapp.com/${url}`);
-        const data = response.data
-        const span = document.createElement('span');
-        span.innerHTML = data;
-        const children = span.children;
-        const text = [];
-        for (let i = 0; i < children.length; i++) {
-            if (hasTextContent(children[i]) && children[i].textContent) {
-                text.push(children[i].textContent.replace(/(<([^>]+)>)/ig, " "));
-            }
-        };
-        const array = text.join(" ").replace(/[^a-zA-Z ]/g, " ").split(" ").filter(n => noNewLines(n));
-        console.log(array);
-        return array;
-    } catch {
-        return [];
-    }
+    const response = await axios.get(`https://cors-anywhere.herokuapp.com/${url}`);
+    const data = response.data
+    const span = document.createElement('span');
+    span.innerHTML = data;
+    const children = span.children;
+    const text = [];
+    for (let i = 0; i < children.length; i++) {
+        if (hasTextContent(children[i]) && children[i].textContent) {
+            text.push(children[i].textContent.replace(/(<([^>]+)>)/ig, " "));
+        }
+    };
+    return text.join(" ").replace(/[^a-zA-Z ]/g, " ").split(" ").filter(n => noNewLines(n));
 }
